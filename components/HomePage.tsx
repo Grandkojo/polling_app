3ac5124272e3ab7f29cd21cf454684dee2f6d384
Grandkojo <essienernest.kojoowusu@gmail.com>
2025-09-22 01:15:25 +0000

    update tag

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -50,7 +50,7 @@ export default function HomePage() {
               <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
-              Trusted by thousands of users worldwide
+              Building the future of polling
             </div>
 
             <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-900 mb-6 leading-tight">
